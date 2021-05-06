6a966e017a219f0bb60933c487f356a6524b7062
Darshan A. Patil <51800177+patildarshan66@users.noreply.github.com>
2021-05-06 15:59:09 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Demo App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,9 +21,10 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
+        primarySwatch: Colors.lightBlue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Demo Home Page'),
     );
   }
 }
@@ -46,18 +48,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // void _incrementCounter() {
+  //   setState(() {
+  //     // This call to setState tells the Flutter framework that something has
+  //     // changed in this State, which causes it to rerun the build method below
+  //     // so that the display can reflect the updated values. If we changed
+  //     // _counter without calling setState(), then the build method would not be
+  //     // called again, and so nothing would appear to happen.
+  //     _counter++;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -68,46 +70,387 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        // title: Text(widget.title),
+        leading: IconButton(
+          icon: Icon(Icons.search),
+          onPressed: () {
+            HapticFeedback.vibrate();
+          },
+        ),
+        centerTitle: true,
+        title: Icon(Icons.ac_unit),
+        actions: [
+          IconButton(
+              icon: Icon(
+                Icons.menu,
+                color: Colors.black54,
+              ),
+              onPressed: () {
+                _scaffoldKey.currentState.openEndDrawer();
+                HapticFeedback.vibrate();
+              })
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      endDrawer: Drawer(
+        child: Scrollbar(
+          controller: _scrollController,
+          thickness: 6,
+          // isAlwaysShown: true,
+          child: SingleChildScrollView(
+            controller: _scrollController,
+            child: SafeArea(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 25.0, bottom: 10),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.grey[200],
+                      child: Icon(
+                        Icons.person,
+                        size: 100,
+                        color: Colors.grey,
+                      ),
+                      radius: 70,
+                    ),
+                  ),
+                  Text(
+                    'Your Name',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 30,
+                    ),
+                    child: Divider(
+                      thickness: 3,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Home',
+                        style: TextStyle(
+                          fontSize: 16,
+                          // fontWeight: FontWeight.bold,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Icon(
+                        Icons.home,
+                        color: Colors.black54,
+                      ),
+                      SizedBox(width: 15),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Xyzzz',
+                        style: TextStyle(
+                          fontSize: 16,
+                          // fontWeight: FontWeight.bold,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Icon(
+                        Icons.storefront,
+                        color: Colors.black54,
+                      ),
+                      SizedBox(width: 15),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Xyzzz',
+                        style: TextStyle(
+                          fontSize: 16,
+                          // fontWeight: FontWeight.bold,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Icon(
+                        Icons.storefront,
+                        color: Colors.black54,
+                      ),
+                      SizedBox(width: 15),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Xyzzz',
+                        style: TextStyle(
+                          fontSize: 16,
+                          // fontWeight: FontWeight.bold,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Icon(
+                        Icons.storefront,
+                        color: Colors.black54,
+                      ),
+                      SizedBox(width: 15),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Xyzzz',
+                        style: TextStyle(
+                          fontSize: 16,
+                          // fontWeight: FontWeight.bold,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Icon(
+                        Icons.storefront,
+                        color: Colors.black54,
+                      ),
+                      SizedBox(width: 15),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Xyzzz',
+                        style: TextStyle(
+                          fontSize: 16,
+                          // fontWeight: FontWeight.bold,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Icon(
+                        Icons.person,
+                        color: Colors.black54,
+                      ),
+                      SizedBox(width: 15),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Xyzzz',
+                        style: TextStyle(
+                          fontSize: 16,
+                          // fontWeight: FontWeight.bold,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Icon(
+                        Icons.settings,
+                        color: Colors.black54,
+                      ),
+                      SizedBox(width: 15),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  Divider(thickness: 1.5),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Favorites',
+                        style: TextStyle(
+                          fontSize: 16,
+                          // fontWeight: FontWeight.bold,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Icon(
+                        Icons.star,
+                        color: Colors.black54,
+                      ),
+                      SizedBox(width: 15),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Share',
+                        style: TextStyle(
+                          fontSize: 16,
+                          // fontWeight: FontWeight.bold,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Icon(
+                        Icons.share,
+                        color: Colors.black54,
+                      ),
+                      SizedBox(width: 15),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Help',
+                        style: TextStyle(
+                          fontSize: 16,
+                          // fontWeight: FontWeight.bold,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Icon(
+                        Icons.info,
+                        color: Colors.black54,
+                      ),
+                      SizedBox(width: 15),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Lock',
+                        style: TextStyle(
+                          fontSize: 16,
+                          // fontWeight: FontWeight.bold,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Icon(
+                        Icons.lock,
+                        color: Colors.black54,
+                      ),
+                      SizedBox(width: 15),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Logout',
+                        style: TextStyle(
+                          fontSize: 16,
+                          // fontWeight: FontWeight.bold,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      SizedBox(width: 20),
+                      Icon(
+                        Icons.exit_to_app,
+                        color: Colors.black54,
+                      ),
+                      SizedBox(width: 15),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
+      endDrawerEnableOpenDragGesture: false,
+      body: Center(
+        child: Text(
+          'Your Content Here....',
+          style: TextStyle(
+            color: Colors.black54,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        items: _kBottmonNavBarItems,
+        currentIndex: _currentTabIndex,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black54,
+        type: BottomNavigationBarType.shifting,
+        onTap: (int index) {
+          setState(() => this._currentTabIndex = index);
+        },
+      ),
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+        onPressed: () {
+          HapticFeedback.vibrate();
+        },
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  int _currentTabIndex = 0;
+
+  static const _kBottmonNavBarItems = <BottomNavigationBarItem>[
+    BottomNavigationBarItem(
+      // backgroundColor: Colors.white,
+      icon: Icon(Icons.home),
+      label: 'Home',
+    ),
+    BottomNavigationBarItem(
+      // backgroundColor: Colors.white,
+      icon: Icon(Icons.whatshot),
+      label: 'XYZ',
+    ),
+    BottomNavigationBarItem(
+      // backgroundColor: Colors.white,
+      icon: Icon(Icons.playlist_play),
+      label: 'PlayList',
+    ),
+    BottomNavigationBarItem(
+      // backgroundColor: Colors.white,
+      icon: Icon(Icons.view_carousel),
+      label: 'View',
+    ),
+    BottomNavigationBarItem(
+      // backgroundColor: Colors.white,
+      icon: Icon(
+        Icons.favorite,
+      ),
+      label: 'Favorite',
+    ),
+  ];
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final ScrollController _scrollController = ScrollController();
 }
